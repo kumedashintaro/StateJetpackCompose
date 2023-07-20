9ea19aaaa2cf704a970762040775ec9a3133775c
kumedashintaro <pii.k2.bys@gmail.com>
2023-07-20 16:06:33 +0900

    fix remember to rememberSaveable

--- app/src/main/java/com/example/statejetpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/statejetpackcompose/WaterCounter.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,7 +17,7 @@ import com.example.statejetpackcompose.ui.theme.StateJetpackComposeTheme
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
             Text(text = "You've had $count glasses.")
