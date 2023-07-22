39a0a4be179426bd90da9fa6cfdb8b43495db9d4
kumedashintaro <pii.k2.bys@gmail.com>
2023-07-22 18:11:25 +0900

    add ViewModel

--- app/src/main/java/com/example/statejetpackcompose/WellnessTasksList.kt
+++ app/src/main/java/com/example/statejetpackcompose/WellnessTasksList.kt
@@ -7,9 +7,10 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
-    modifier: Modifier = Modifier,
     list: List<WellnessTask>,
-    onCloseTask: (WellnessTask) -> Unit
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier
@@ -18,7 +19,11 @@ fun WellnessTasksList(
             items = list,
             key = { task -> task.id }
         ) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
         }
     }
 }
