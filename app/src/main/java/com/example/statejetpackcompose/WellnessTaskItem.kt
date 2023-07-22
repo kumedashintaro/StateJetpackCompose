abb7dc229903655b8c9b4e35e42721c290df7beb
kumedashintaro <pii.k2.bys@gmail.com>
2023-07-22 15:17:20 +0900

    add remove task

--- app/src/main/java/com/example/statejetpackcompose/WellnessTaskItem.kt
+++ app/src/main/java/com/example/statejetpackcompose/WellnessTaskItem.kt
@@ -20,14 +20,18 @@ import androidx.compose.ui.unit.dp
 import com.example.statejetpackcompose.ui.theme.StateJetpackComposeTheme
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = {}, // we will implement this later!
+        onClose = onClose,
         modifier = modifier,
     )
 }
